b728484568d36aa8804dbb8dcc2041ba8ebdee00
オノヤスヒロ <47861810+cant-dev@users.noreply.github.com>
2021-03-22 23:12:21 +0900

    タスクの追加 イベントの設定、State(変数と関数)で管理する

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,44 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState(["AAAA", "BBBB"]);
   const [completeTodos, setCompleteTodos] = useState(["CCCC"]);
+
+  // renderできるように、変数で渡してevent + target + 変数 で渡す
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const OnclickAdd = () => {
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+  };
+  // 未完了のTodoから削除する
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+    alert(index);
+  };
   return (
     <>
       <div className="input-area">
-        <input placeholder="Todoを入力"></input>
-        <button>追加</button>
+        <input
+          placeholder="Todoを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        ></input>
+        <button onClick={OnclickAdd}>追加</button>
       </div>
 
       <div className="incomplete-area">
         <p className="title">未完了のTodo</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
