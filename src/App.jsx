6e77264aa2dfd6f48bac74e23bc219019c0363be
オノヤスヒロ <47861810+cant-dev@users.noreply.github.com>
2021-03-23 14:08:08 +0900

    Task_Completeまで 配列を新規に作り直して、State関数で状態を更新する

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,38 @@ import "./styles.css";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setIncompleteTodos] = useState(["AAAA", "BBBB"]);
+  // 第一変数は初期値であり、render後にセットされる変数、第二関数は第一変数をstate(更新)する。
+  // useStateの中身は第一変数
+  const [incompleteTodos, setIncompleteTodos] = useState(["aaaa", "bbbb"]);
   const [completeTodos, setCompleteTodos] = useState(["CCCC"]);
 
   // renderできるように、変数で渡してevent + target + 変数 で渡す
   const onChangeTodoText = (event) => setTodoText(event.target.value);
 
   const OnclickAdd = () => {
+    if (todoText === "") return;
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
+    setTodoText("");
   };
   // 未完了のTodoから削除する
   const onClickDelete = (index) => {
     const newTodos = [...incompleteTodos];
+    // 何番目の要素に対して, いくつ削除するのか？というメソッド
     newTodos.splice(index, 1);
+    // 要素の更新
     setIncompleteTodos(newTodos);
-    alert(index);
   };
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    // 何番目の要素に対して, いくつ削除するのか？というメソッド
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -38,7 +53,7 @@ export const App = () => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
                 <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
